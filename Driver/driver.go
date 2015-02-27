de3a84221625e0b5e30818ace6bde3df0beb2911
Saarots <eiristh@stud.ntnu.no>
2015-02-27 11:09:11 +0100

    Update driver.go
    Fiksing av else problem og lage siste funksjoner

--- Driver/driver.go
+++ Driver/driver.go
@@ -1,13 +1,15 @@
 package Driver // where "driver" is the folder that contains io.go, io.c, io.h, channels.go, channels.c and driver.go
+package Driver // where "driver" is the folder that contains io.go, io.c, io.h, channels.go, channels.c and driver.go
 
 /*
 #cgo LDFLAGS: -lcomedi -lm
 #include "io.h"
 */
 
-import  "C"
-		."time"
-		."math"
+import  (
+		"time"
+		"math"
+		)
 
 //Constants
 
@@ -32,21 +34,21 @@ var buttonMatrix = [N_FLOORS][N_BUTTONS] int {
 }
 
 
-func ElevStartUp() {
+func ElevStartUp() int {
 	//Initialization og hardware
 	if(IoInit() != nil){return 0}
 
 	//Turing off all lamps
 	for i := 0; i < N_FLOORS; i++{
-		if(i != 0){SetButtonLamp(BUTTON_CALL_DOWN, i, false)
+		if(i != 0){SetButtonLamp(BUTTON_CALL_DOWN, i, false)}
 		if(i != N_FLOORS-1){SetButtonLamp(BUTTON_CALL_UP, i, false)}
 		SetButtonLamp(BUTTON_COMMAND, i, false)
 	}
 
-	//Turing off 
+	//Turing off ligths
 	SetStopLamp(false)
 	SetDoorLamp(false)
-	floorLamp()
+	go floorLamp()
 
 
 
@@ -55,77 +57,106 @@ func ElevStartUp() {
 
 
 
-// gets current floor
+
+// Gets current floor
 func GetCurrentFloor() int {
-	if(IoReadBit(SENSOR1)){return = 0}
-	else if (IoReadBit(SENSOR2)){return =1}
-	else if (IoReadBit(SENSOR3)){return =2}
-	else if (IoReadBit(SENSOR4)){return =3}
-	else {return = -1}
+	if(IoReadBit(SENSOR1)){
+		return 0
+	}else if (IoReadBit(SENSOR2)){
+		return 1
+	}else if (IoReadBit(SENSOR3)){
+		return 2
+	}else if (IoReadBit(SENSOR4)){
+		return 3
+	}else {
+		return -1
+	}
 }
-//
-// Get button
-// Get stop button
-//
+
 // Settin floor lamp    Denne kan vi kjøre i bakgrunnen???
 func floorLamp() {
 
 	floor = GetCurrentFloor()
 
 	if (floor != -1){
-		if ((floor & 0x02) != 0){IoSetBit(FLOOR_IND1)}
-		else {IoClearBit(FLOOR_IND1)}
-		if ((floor & 0x01) != 0) {IoSetBit(FLOOR_IND2)}
-		else {IoClearBit(FLOOR_IND2)}
+		if ((floor & 0x02) != 0){
+			IoSetBit(FLOOR_IND1)
+		}else {
+			IoClearBit(FLOOR_IND1)
+		}
+		if ((floor & 0x01) != 0) {
+			IoSetBit(FLOOR_IND2)
+		}else {
+			IoClearBit(FLOOR_IND2)
+		}
 	}
-	//go floorLamp()
+	// Legge inn en sleep her?
 }
 
-func readButtons() {
-	for i :=0 i < N_FLOORS; i++){
+// Reads obstruction
+func readObstruction() int {
+	return IoReadBit(OBSTRUCTION)
+}
+
+// Reads stop button
+func readStopButton() int {
+	return IoReadBit(STOP)
+}
+
+func readButtons() { // Ta med lesing av stopp og obstruksjon her og?
+	for i :=0; i < N_FLOORS; i++ {
 		for j := 0; j < N_BUTTONS; j++ {
-			if(IoReadBit(buttonMatrix[i][j])){SetButtonLamp(i,j,true)}
+			if(IoReadBit(buttonMatrix[i][j])){SetButtonLamp(i,j,true)} //Setter lys med en gang, vil vi det?
 		}
+	}
 }
-	
-}
+
 // Setting button lamp
 func SetButtonLamp(floor int, button int, on bool) {  //// ON er et dårlig navn!!!
-	if (on){IoSetBit(lampMatrix[floor][button])}
-	else {IoClearBit(lampMatrix[floor][button])}
+	if (on){
+		IoSetBit(lampMatrix[floor][button])
+	}else {
+		IoClearBit(lampMatrix[floor][button])
+	}
 }
 
 //Setting stop lamp
 func SetStopLamp(stop bool) {
-	if(stop){IoSetBit(LIGHT_STOP)}
-	else {IoClearBit(LIGHT_STOP)}
+	if(stop){
+		IoSetBit(LIGHT_STOP)
+	}else {
+		IoClearBit(LIGHT_STOP)
+	}
 }
 
 //Setting door lamp
 func SetDoorLamp(door bool) {
-	if(door){IoSetBit(DOOR_OPEN)}
-	else {IoClearBit(DOOR_OPEN)}
+	if(door){
+		IoSetBit(DOOR_OPEN)
+	}else {
+		IoClearBit(DOOR_OPEN)
+	}
 }
-
-
 
 // may need "speed variable" to stop det elevator or use "dir"
 func SetMotorDir(dir int) {
 
 	//Stopping the elevator
 	if(dir == 0){
-		if(lastDir == 1){IoSetBit(MOTORDIR)}
-		else if(lastDir == -1){IoClearBit(MOTORDIR)}
-		Sleep(10*Millisecond) // usikker på om millisecond er en variable
+		if(lastDir == 1){
+			IoSetBit(MOTORDIR)
+		}else if(lastDir == -1){
+			IoClearBit(MOTORDIR)
+		}
+		time.Sleep(10*time.Millisecond)
+	}else if(dir == 1){ //Starting the elevator going up
+		IoClearBit(MOTORDIR)
+	}else if(dir == -1){//Starting the elevator going down
+		IoSetBit(MOTORDIR)
 	}
-	//Starting the elevator going up
-	else if(dir == 1){IoClearBit(MOTORDIR)}
-	//Starting the elevator going down
-	else if(dir == -1){IoSetBit(MOTORDIR)}
-
 	lastDir = dir
 	//Writing new speed to motor
-	speed = Abs(dir*(300)) //ANNER IKKE HVA SOM ER NORMAL FART!!!
+	speed = math.Abs(dir*(300)) //ANNER IKKE HVA SOM ER NORMAL FART!!!
 	IoWriteAnalog(MOTOR, int(2048+4*float64(speed)))
 	
 }
